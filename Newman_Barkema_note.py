f36b02ddfcc443caaa38c9cb36eefb1f62cd60d1
Shawn Zhang <Shawn1776@users.noreply.github.com>
2016-11-15 16:32:43 -0500

    v1

---
+++ Newman_Barkema_note.py
@@ -0,0 +1,36 @@
+#chapter1
+'''
+1) basic concepts:
+
+1.1) ground state
+1.2) degenerate state
+1.3) Hamiltonian system (H=KE+V) 
+
+
+2) key concepts:(need to be memorized)
+2.1) microcanonical ensemble: [NVE system]
+For a classical system which has a continuum of energy states there can be a continuous
+set of degenerate states through which the system passes, and an average over those states
+can sometimes give a good answer for certain properties of the system. Such sets of
+degenerate states are said to form a microcanonical ensemble. 
+
+2.2) canonical ensembe[NVT system, with a heat bath]
+The more general case we consider here, in which there is a thermal reservoir causing the energy of the system
+to fluctuate, is known as a canonical ensemble.
+
+2.3) master equationS from one state to anther
+
+dwµ/dt = Sum[wν(t)R(ν → µ) − wµ(t)R(µ → ν)], where Sum(wu)=1
+
+
+
+
+3) English:
+3.1) synopsis: summary, outline, main idea
+3.2) paradim: model
+3.3) continuum: a continuum of energy states
+3.4) sheer: completed, sheer number of 
+3.5) probabilistic treatment
+
+
+'''
